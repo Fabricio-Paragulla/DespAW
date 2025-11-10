e83649bfda827c81b63781e7337defdef9f34447
root <prueba@gmail.com>
2025-11-10 14:18:07 +0100

    organizacion con directorios

--- Docker/Fabricio/dockerfiles/base/ubuntu/admin/start.sh
+++ Docker/Fabricio/dockerfiles/base/ubuntu/admin/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-tail -f /dev/null
+newUser(){
+    useradd -rm -d /home/fabri -s /bin/bash fabri
+    echo "fabri:1234" | chpasswd
+    echo "Bienvenido Fabricio ... " > /home/fabri/bienvenido.txt
+}
+
+main(){
+    newUser
+    tail -f /dev/null
+}
+
+main
